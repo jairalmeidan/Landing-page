c9f01cb3a590717002aad53ee983555909230e6d
jairalmeidan <policate83@gmail.com>
2024-12-19 19:35:09 -0300

    primeito commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        azul1: {
+          escuro: '#1E3A5F',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
